f666f549a30621f3c59b7206e787f9dc8ea89438
Sougata Ghosh <sougataghosh314@gmail.com>
2021-04-30 14:36:02 +0530

    some changes

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:]) {
-  echo "Hello, ${config.name}. Today is ${config.dayOfWeek} and you're learning ${config.topic}."
+  if(config.name && config.dayOfWeek && config.topic) {
+	echo "Hello, ${config.name}. Today is ${config.dayOfWeek} and you're learning ${config.topic}."
+  } else {
+    echo "You didn't provide me all the variables!"
+  }
 }
 
